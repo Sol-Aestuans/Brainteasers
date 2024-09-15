5bc70cc248eafb844ee68c580370de04bb96803b
Sol-Aestuans <aidansol@outlook.com>
2024-09-14 20:31:00 -0700

    isAnagram completed with sorting solution

---
+++ isAnagram.java
@@ -0,0 +1,26 @@
+/*
+Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+
+Input: s = "racecar", t = "carrace"
+
+Output: true
+ */
+
+ class Solution {
+    public boolean isAnagram(String s, String t) {
+        // sort and compare
+        char sTemp[] = s.toCharArray();
+        char tTemp[] = t.toCharArray();
+        Arrays.sort(sTemp);
+        Arrays.sort(tTemp);
+
+        s = new String(sTemp);
+        t = new String(tTemp);
+
+        return s.equals(t);
+    }
+}
