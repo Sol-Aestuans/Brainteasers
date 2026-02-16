63e04e16803e0d7c2ccea0f3a382d80da67302f1
Sol <aidansol@outlook.com>
2026-02-15 22:26:58 -0800

    stringCompression

--- stringCompression.py
+++ stringCompression.py
@@ -1,27 +1,33 @@
 class Solution:
-    def compress(self, chars) -> int:
+  def compress(self, chars: list[str]) -> int:
+    ans = 0
+    i = 0
 
-        running_total = 1
+    while i < len(chars):
+      letter = chars[i]
+      count = 0
+      
+      ## count duplicates
+      while i < len(chars) and chars[i] == letter:
+        count += 1
+        i += 1
+      chars[ans] = letter
+      ans += 1
 
-        for i in range(0, len(chars)):
-            cur = chars[i]
-            if len(chars) == 0 or isinstance(chars[i-1], int) or output[-1] != cur:
-                output.append(cur)
-            elif cur == output[-1]:
-                running_total += 1
-            if i == len(chars) - 1 or chars[i + 1] != cur:
-                running_total_str = str(running_total)
-                if len(running_total_str) > 1:
-                    for char in running_total_str:
-                        output.append(char)
-                elif running_total > 1:
-                    output.append(str(running_total))
-                running_total = 1
-                
-        return output
+      ## encode numbers
+      if count > 1:
+        for c in str(count):
+          chars[ans] = c
+          ans += 1
+        
+        # remove duplicate chars and adjust i
+        j = ans
+        while j < len(chars) and chars[j] == letter:
+          chars.pop(j)
+          i-=1
 
+    return ans
 if __name__ == "__main__":
     chars = ["a","a","b","b","b","b","b","b","b","b","b","b","b"]
-    sol = Solution()
-    result = sol.compress(chars)
-    print("Compression string: ", result)
+    result = Solution().compress(chars)
+    print("String: " + "".join(chars) + "\nCompressed length:" + str(result))
